9d5707f43d8f2a9d8e4d59c1c591c77f726a40a5
Colton Martin <cjmartin@localhost.localdomain>
2020-05-06 15:45:29 -0500

    Diffie working with Clock Crypto

--- Diffie_Hellman.py
+++ Diffie_Hellman.py
@@ -5,60 +5,97 @@
 #in this case, p is for curve25519 (2 ^ 55 - 19)
 import time
 import os
+import threading
 from ECC import ( Fp )
-standardized_point = (Fp(0), Fp(1))
-if os.fork() == 0 :
-    #Bob
-    secret_key = 123
-    public_key = Fp.scalarMult_Edwards( secret_key, standardized_point ) 
-    #send public key to file 'OnTheWire.txt'
-    print( '(Bob) Sending Public key ', public_key, '...' )
-    x, y = public_key
-    outFile = open( 'OnTheWire.txt', 'w' )
-    outFile.write( str(x.int) )
-    outFile.close()
-    time.sleep(3)
-    outFile = open( 'OnTheWire.txt', 'w' )
-    outFile.write( str(y.int) )
-    outFile.close()
-    #wait for Alice to send public key
-    '''
-    time.sleep(8)
-    inFile = open( 'OnTheWire.txt', 'r' )
-    x = Fp(int(inFile.read()))
-    y = Fp(int(inFile.read()))
-    incoming_public_key = (Fp( x ), Fp( y ))
-    shared_secret = Fp.scalarMult_Edwards( secret_key, incoming_public )
-    inFile.close()
-    print( '(Bob) The shared secret is ', shared_secret )
-    '''
-else : 
-    #Alice
-    secret_key = 321
-    public_key = Fp.scalarMult_Edwards( secret_key, standardized_point ) 
-    #Wait for Bob to send public key
-    time.sleep(2)
-    #Get Bob's public key
-    inFile = open( 'OnTheWire.txt', 'r' )
-    x = Fp(inFile.read())
-    inFile.close()
-    time.sleep(2)
-    inFile = open( 'OnTheWire.txt', 'r' )
-    y = Fp(inFile.read())
-    inFile.close()
-    print( x )
-    print( y )
-    '''
-    incoming_public_key = (Fp( x ), Fp( y ))
 
-    inFile.close()
-    print( '(Alice) I received Bob\'s public key : ', incoming_public_key )
-    shared_secret = Fp.scalarMult_Edwards( secret_key, incoming_public_key )
-    print( '(Alice) The shared secret is ', shared_secret )
-    print( '(Alice) Sending Public key ', public_key, '...' )
-    outFile = open('OnTheWire.txt', 'w')
+s = threading.Semaphore(1)
+standardized_point = ( Fp(39984455814760748732201855760812543180582291579854007269875196977732502578790), Fp(4/5) )
+if os.fork() == 0 :
+    #Bob's keys
+    secret_key = 1
+    print(standardized_point)
+    public_key = Fp.scalarMult_Edwards( secret_key, standardized_point )
+
+    #send public key to Alice
+    s.acquire()
+    print( '(Bob) Sending Public key ...\n', public_key, '...' )
+
+    outFile = open( 'OnTheWire.txt', 'w' )
     x, y = public_key
-    outFile.writes( str(x.int) )
-    outFile.writes( str(y.int) )
+    outFile.writelines( str(x.int) )
+    outFile.writelines( '\n' )
+    outFile.writelines( str(y.int) )
+    outFile.writelines( '\n' )
     outFile.close()
-    '''
+    s.release()
+
+    #wait for Alice to send public key
+    time.sleep(0.5)
+
+    #Get public key from Alice
+    s.acquire()
+    inFile = open( 'OnTheWire.txt', 'r' )
+    temp = inFile.read(1)
+    x = ''
+    y = ''
+    while temp != '\n' :
+        x = x + temp
+        temp = inFile.read(1)
+    temp = inFile.read(1)
+    while temp != '\n' :
+        y = x + temp
+        temp = inFile.read(1)
+    x = int( x )
+    y = int( y )
+    incoming_public_key = ( Fp(x), Fp(y) )
+    inFile.close()
+    s.release()
+    print( '(Bob) I received Alice\'s public key : \n', incoming_public_key )
+    shared_secret = Fp.scalarMult_Edwards( secret_key, incoming_public_key )
+    print( '(Bob) The shared secret is ...\n', shared_secret )
+else : 
+    #Alice's keys
+    #32 bit integer 
+    secret_key = 3
+    public_key = Fp.scalarMult_Edwards( secret_key, standardized_point )
+
+    #Wait for Bob to send public key
+    time.sleep(.25)
+
+    #Get Bob's public key
+    s.acquire()
+    inFile = open( 'OnTheWire.txt', 'r' )
+    temp = inFile.read(1)
+    x = ''
+    y = ''
+    while temp != '\n' :
+        x = x + temp
+        temp = inFile.read(1)
+    temp = inFile.read(1)
+    while temp != '\n' :
+        y = x + temp
+        temp = inFile.read(1)
+    x = Fp(int( x ))
+    y = Fp(int( y ))
+    incoming_public_key = ( x, y )
+    inFile.close()
+    s.release()
+    #Assign x, y values to key
+    print( '(Alice) I received Bob\'s public key ...\n', incoming_public_key )
+    shared_secret = Fp.scalarMult_Edwards( secret_key, incoming_public_key ) 
+    print( '(Alice) The shared secret is ...\n', shared_secret )
+
+    s.acquire()
+    print( '(Alice) Sending Public key ...\n', public_key )
+    outFile = open( 'OnTheWire.txt', 'w' )
+    x, y = public_key
+    outFile.writelines( str(x.int) )
+    outFile.writelines( '\n' )
+    outFile.writelines( str(y.int) )
+    outFile.writelines( '\n' )
+    outFile.close()
+    s.release()
+    
+    #Wait to allow child process to finish
+    time.sleep(3)
+'''
--- ECC.py
+++ ECC.py
@@ -1,8 +1,9 @@
 # in file ECC.py
 # Class to implement Elliptic Curve and Clock Cryptography
-
-#p = 2 ** 255 - 19
-p = 1000003
+import math
+#p = 2 ** 255 - 19 #for use of curve25519 (type of Montgomery, Diffie_Hellman.py) 
+#maxnbits = 32 #for Montgomery
+p = 1000003 #for SecNumCracker.py
 class Fp :
     def __init__( self, x ) : 
         self.int = x % p
@@ -11,21 +12,29 @@ class Fp :
     def setp( x ) : 
         p = x
         return x
+    def add(x, self):
+        return Fp( x + self.int )
+    def Ladd(self, x):
+        return Fp( self.int + x )
+    def sub(x, self):
+        return Fp( x - self.int )
+    def Lsub(self, x):
+        return Fp( self.int - x )
+    def mul(x, self):
+        return Fp( x * self.int )
+    def Lmul(self, x):
+        return Fp( self.int * x )
+    def exp(self, x):
+        return Fp( self.int ^ x )
     __repr__ = __str__
     __eq__ = \
             lambda a,b: a.int == b.int
     __add__ = \
             lambda a,b: Fp( a.int + b.int )
-    def add(x, self):
-        return Fp( x + self.int )
     __sub__ = \
             lambda a,b: Fp( a.int - b.int )
-    def sub(x, self):
-        return Fp( x - self.int )
     __mul__ = \
             lambda a,b: Fp( a.int * b.int )
-    def mul(x, self):
-        return Fp( x * self.int )
     __floordiv__ = \
             lambda a,b: Fp( a.int // b.int )
     __truediv__ = \
@@ -33,6 +42,7 @@ class Fp :
 #Overload operators to hangle Cartesian Coordinates
 #Add Cartesian Coordinates together
     def clockadd( P1, P2 ) :
+        #x^2 + y^2 = 1
         x1, y1 = P1
         x2, y2 = P2
         x3 = x1 * y2 + y1 * x2
@@ -40,7 +50,8 @@ class Fp :
         return x3, y3
 #Adds points of an Edwards curve
     def edwardsadd(P1,P2) :
-        d = 30 #d cannot be a square
+        #x^2 + y^2 = 1 + d * x^2 * y^2
+        d = 121655 / 121666 #d cannot be a square
         #p needs to be large prime number
         x1,y1 = P1
         x2,y2 = P2
@@ -48,17 +59,82 @@ class Fp :
         y3 = (y1*y2-x1*x2)/(Fp.sub( 1, Fp.mul( d, (x1*x2*y1*y2))))
         return x3,y3
 #Multplies a Cartesian Coordinate by a scaler
-    def scalarMult_Edwards ( n, P ) :
-        if n == 0 : return ( Fp( 0 ), Fp( 1 ) )
-        if n == 1 : return P
-        Q = Fp.scalarMult_Edwards( n // 2, P )
-        Q = Fp.edwardsadd( Q, Q )
-        if n % 2 : Q = Fp.edwardsadd( P, Q )
-        return Q
     def scalarMult_Clock ( n, P ) :
+        #x^2 + y^2 = 1
         if n == 0 : return ( Fp( 0 ), Fp( 1 ) )
         if n == 1 : return P
         Q = Fp.scalarMult_Clock( n // 2, P )
         Q = Fp.clockadd( Q, Q )
         if n % 2 : Q = Fp.clockadd( P, Q )
         return Q
+    def scalarMult_Edwards ( n, P ) :
+        #x^2 + y^2 = 1 + d * x^2 * y^2
+        if n == 0 : return ( Fp( 0 ), Fp( 1 ) )
+        if n == 1 : return P
+        Q = Fp.scalarMult_Edwards( n // 2, P )
+        Q = Fp.edwardsadd( Q, Q )
+        if n % 2 : Q = Fp.edwardsadd( P, Q )
+        return Q
+    def cswap( x, y, bit ) :
+        if bit == 1 :
+            temp = x
+            x = y
+            y = temp
+        return x, y
+    def scalarMult_Montgomery(n,x1):
+        # B * y^2 = x^3 + A * x^2 + x
+        A = 486662
+        x2,z2,x3,z3 = Fp(1),Fp(0),x1,Fp(1)
+        for i in reversed(range(maxnbits)):
+            bit = 1 & (n >> i)
+            x2,x3 = Fp.cswap(x2,x3,bit)
+            z2,z3 = Fp.cswap(z2,z3,bit)
+            x3,z3 = (Fp.exp( (x2*x3-z2*z3),2),x1*Fp.exp(x2*z3-z2*x3,2))
+            x2 = Fp.exp((Fp.exp(x2,2) - Fp.exp(z2,2)),2)
+            z2 = Fp.mul(4,x2)*z2*(Fp.exp(x2,2)+Fp.mul(A,x2)*z2+Fp.exp(z2,2))
+            x2,x3 = Fp.cswap(x2,x3,bit)
+            z2,z3 = Fp.cswap(z2,z3,bit)
+        return x2*Fp.exp(z2,(p-2))
+#Testing Montgomer, can't 
+standardized_point = (Fp(1000), Fp(2))
+Bsk = 123
+Bpk = Fp.scalarMult_Clock( Bsk, standardized_point )
+print( '(Bob) Sending Public key ...\n', Bpk, '...' )
+Ask = 333
+Apk = Fp.scalarMult_Clock( Ask, standardized_point )
+alice_secret = Fp.scalarMult_Clock( Ask, Bpk )
+print( '(Alice) The shared secret is ...\n', alice_secret )
+print( '(Alice) Sending Public key ...\n', Apk )
+bob_secret = Fp.scalarMult_Clock( Bsk, Apk )
+print( '(Bob) The shared secret is ...\n', bob_secret )
+'''
+x = 20000001
+y = 20000000
+print( bin(x), bin(y) )
+x, y = Fp.cswap( Fp(x), Fp(y), 1)
+print( bin(x.int), bin(y.int) )
+x = 23894781
+y = 34271799
+print( bin(x), bin(y) )
+x, y = Fp.cswap( Fp(x), Fp(y), 0)
+print( bin(x.int), bin(y.int) )
+bp = Fp(9)
+kBob =   2009478133
+kAlice = 2027179928
+BobPublic = Fp.scalarMult_Montgomery(kBob, bp)
+AlicePublic = Fp.scalarMult_Montgomery(kAlice, bp)
+sharedBob = Fp.scalarMult_Montgomery( kBob * AlicePublic.int ,bp )
+sharedAlice = Fp.scalarMult_Montgomery( kAlice * BobPublic.int,bp  )
+print( sharedBob )
+print( sharedAlice )
+print( '' )
+sharedBob = Fp.scalarMult_Montgomery( kBob , AlicePublic)
+sharedAlice = Fp.scalarMult_Montgomery( kAlice, BobPublic )
+print( sharedBob )
+print( sharedAlice )
+print( '' )
+sharedBob = Fp.scalarMult_Montgomery(  bp.int, Fp(kBob * AlicePublic.int) )
+sharedAlice = Fp.scalarMult_Montgomery( bp.int, Fp(kAlice * BobPublic.int) )
+print( sharedBob )
+print( sharedAlice )
+'''
